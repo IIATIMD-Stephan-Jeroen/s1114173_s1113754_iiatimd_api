3f31c53976787f222af265c403b51c41950e8d74
StephanV2 <stephanhouwaart@hotmail.com>
2021-06-30 21:43:42 +0200

    Tussen commit anders mag ik niet switchen

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -39,12 +39,12 @@ class AuthController extends Controller
         $user = Auth::user();
         //generates a JWT token using sanctum
         $token = $user->createToken('token')->plainTextToken;
-        
+
         // return $token;
         //stores the JWT token in the http request as a cookie for security purposes
         // $cookie = cookie('jwt', $token, 60 * 24);
 
-        //returns response with cookie
+        //returns response with data
         return response([
             'message' => 'Login successfull',
             'message' => $token
